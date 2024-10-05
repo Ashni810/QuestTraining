1d8eb81f0f33db9f25c99cf633ff7955f84d685f
ASHNI ASHOK PP <ashnimalu@gmail.com>
2024-10-05 23:41:06 +0530

    Assignments

---
+++ Assignments/A1/A1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build a rectangle like this  * * * * * * * * * * *
+            //   *                   *
+            //       *                   *
+            //    * * * * * * * * * * *
+            //    
+
+
+            
+            int width = 10;
+            Console.WriteLine("Enter the Height of the Rectangle : ");
+            int height = int.Parse(Console.ReadLine());
+
+            // For Drawing the rectangle
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+   
---
+++ Assignments/A2/A2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "10_202_34_12";
+            int sum = 0;
+            int num = 0;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] == '_')
+                {
+                    sum += num;
+                    num = 0;
+                }
+                else
+                {
+                    num = num * 10 + (pattern[i] - '0');
+                }
+            }
+
+            sum += num;
+            Console.WriteLine(sum); // Output: 258
+        }
+    }
+}
