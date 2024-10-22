e29bdd55a60eb08a8c4107cb2a0dd475eb30c9f4
ASHNI ASHOK PP <ashnimalu@gmail.com>
2024-10-22 21:57:15 +0530

    upadated again

--- Assignments/DAY15/Q20/Program.cs
+++ Assignments/DAY15/Q20/Program.cs
@@ -10,6 +10,13 @@ namespace Q20
     {
         static void Main(string[] args)
         {
+            var names = new List<string>() { "Amal", "Arya", "John", "Diana" };
+            var marks = new List<int> { 65, 70, 100, 90 };
+            var zip = names.Zip(marks, (name, mark) => { return new { name, mark }; });
+            foreach (var name in zip)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
--- Day 16/BooksManagment/Program.cs
+++ Day 16/BooksManagment/Program.cs
@@ -1,34 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace BooksManagment
+namespace BookInserter
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Book One", Author = "Author A", Year = 2001, Genre = "Fiction" },
+                new Book { Id = 2, Title = "Book Two", Author = "Author B", Year = 2002, Genre = "Non-Fiction" },
+                new Book { Id = 3, Title = "Book Three", Author = "Author C", Year = 2003, Genre = "Science Fiction" },
+                new Book { Id = 4, Title = "Book Four", Author = "Author D", Year = 2004, Genre = "Fantasy" },
+                new Book { Id = 5, Title = "Book Five", Author = "Author E", Year = 2005, Genre = "Mystery" }
+            };
+
+            InsertBooksIntoDatabase(books);
+        }
+
+        public static void InsertBooksIntoDatabase(List<Book> books)
+        {
             var connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ashni\OneDrive\Documents\QuestDb.mdf;Integrated Security=True";
 
-            var createTableQuery = @"CREATE TABLE Books
-                     (
-                        Id INT PRIMARY KEY,
-                        Title VARCHAR(255) NOT NULL,
-                        Author VARCHAR(255) NOT NULL,
-                        Year INT NOT NULL,
-                        Genre VARCHAR(100) NOT NULL
-                   )";
-            var conn = new SqlConnection(connStr);
-            conn.Open();
-            var command = new SqlCommand(createTableQuery, conn);
-            command.ExecuteNonQuery();
-            conn.Close();
+            using (var connection = new SqlConnection(connStr))
+            {
+                connection.Open();
 
-        }    
+                foreach (var book in books)
+                {
+                    var query = "INSERT INTO Books (Id, Title, Author, Year, Genre) VALUES (@Id, @Title, @Author, @Year, @Genre)";
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id", book.Id);
+                        command.Parameters.AddWithValue("@Title", book.Title);
+                        command.Parameters.AddWithValue("@Author", book.Author);
+                        command.Parameters.AddWithValue("@Year", book.Year);
+                        command.Parameters.AddWithValue("@Genre", book.Genre);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
     }
 }
+
